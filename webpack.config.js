efadf784f786a7879bd67a775f8f9525744b926c
Michael Schinis <mschinis@gmail.com>
2017-10-21 22:10:44 +0100

    feat(webpack): Adds babel transformation

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {}
+                }
+            },
+            { test: /\.json$/, loader: 'json-loader' }
+        ]
+    },
+    resolve: {
+        alias: {
+
+            src: path.resolve(__dirname, './src')
+        }
+    },
+    node: {
+        console: true,
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    }
+};
