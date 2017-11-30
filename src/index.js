f950086e25ca52d769687c4e2f52246b864645b7
Michael Schinis <mschinis@gmail.com>
2017-11-30 05:50:13 +0000

    feat(app): Finalises algolia indexing.

--- src/index.js
+++ src/index.js
@@ -1,96 +1,95 @@
 require('dotenv').config();
 
-import Bluebird from 'bluebird';
 import logger from 'src/utils/logger';
 import drivers from 'src/drivers';
-import { request } from 'src/utils/request';
+import { readTmpFile, readTmpFileAsync } from 'src/utils/fs';
+import schemas from './schemas';
 
 const {
     DRIVER,
-    DEBUG,
-    FASTLY_ENDPOINT: fastlyEndpoint
+    DEBUG
 } = process.env;
 
-if(DEBUG) logger.logGreen(`Started downloading assets from:: ${fastlyEndpoint}`);
+// Initialise driver
+drivers[DRIVER].init('modules');
+drivers[DRIVER].init('classes');
+drivers[DRIVER].init('methods');
 
-function randomPromiseTimer() {
-    return new Bluebird.Promise(resolve => {
-        setTimeout(resolve, Math.random() * 150);
-    });
-}
-// Start downloading json files
-request(`${fastlyEndpoint}/rev-index/ember.json`)
+// Load ember.json which includes all available ember versions.
+readTmpFileAsync('rev-index/ember.json')
+// Extract available versions
 .then(emberJson => emberJson.meta.availableVersions)
 // Grab the json file of each ember version
 .map(version => {
-    const emberVersionJson = `${fastlyEndpoint}/rev-index/ember-${version}.json`;
-
-    logger.logBlue(`DOWNLOADING:: ${emberVersionJson}`);
-
-    return request(emberVersionJson)
-    .then(res => {
-        if(DEBUG) {
-            drivers.json.write(`rev-index/${res.data.attributes.version}/version.json`, res);
-        }
-
-        logger.logBlue(`COMPLETED:: ${emberVersionJson}`);
-        return res;
-    });
+    const emberVersionJSONPath = `rev-index/ember-${version}.json`;
+    logger.logBlue(`OPENING:: ${emberVersionJSONPath}`);
+    return readTmpFile(emberVersionJSONPath);
 })
+// Fetch all public modules and public classes
 .map(version => {
     const publicModules = version.data.relationships['public-modules'].data
         .map(module => {
-            // encode uri component of the public module, to grab the
-            // module path on fastly from the "meta.module" object
+            // Module names are uri encoded
             const id = encodeURIComponent(module.id);
-            const modulePath = version.meta.module[id];
-            return `${fastlyEndpoint}/json-docs/ember/${version.data.attributes.version}/modules/${modulePath}.json`
-        })
-        // .map(randomPromiseTimer)
-        .map(request);
+            const modulePath = `json-docs/ember/${version.data.attributes.version}/modules/${version.meta.module[id]}.json`;
+
+            logger.logBlue(`OPENNING:: ${modulePath}`);
+            return readTmpFile(modulePath);
+        });
 
     const publicClasses = version.data.relationships['public-classes'].data
         .map(classObj => {
-            // encode uri component of the public class, to grab the
-            // class path on fastly from the "meta.class" object
+            // Class names are uri encoded
             const id = encodeURIComponent(classObj.id);
-            const classPath = version.meta.class[id];
+            const classPath = `json-docs/ember/${version.data.attributes.version}/classes/${version.meta.class[id]}.json`;
 
-            return `${fastlyEndpoint}/json-docs/ember/${version.data.attributes.version}/classes/${classPath}.json`
-        })
-        // .then(randomPromiseTimer)
-        .map(request);
+            logger.logBlue(`OPENNING:: ${classPath}`);
+            return readTmpFile(classPath);
+        });
 
-    return Bluebird.props({
+    return {
         version,
-        publicModules: Bluebird.all(publicModules),
-        publicClasses: Bluebird.all(publicClasses)
-    });
+        publicModules,
+        publicClasses
+    };
 })
-// Remove modules and classes which requests have failed for
+// Run the schema against all data stored
 .map(versionObject => {
     return {
         ...versionObject,
-        publicModules: versionObject.publicModules.filter(m => m && m.attributes && m.attributes.name),
-        publicClasses: versionObject.publicClasses.filter(c => c && c.attributes && c.attributes.name)
+
+        methods: extractMethodsFromClasses(versionObject.publicClasses),
+        publicModules: versionObject.publicModules.map(schemas.moduleSchema),
+        publicClasses: versionObject.publicClasses.map(schemas.classSchema)
     };
 })
-// Write out to json & algolia
+// Write out to selected driver.
 .map(versionObject => {
-    console.log('fulfilled bluebird props');
-    console.log(`map ${versionObject.version.id}`);
-    if(DEBUG) {
-        versionObject.publicModules
-            .forEach(publicModule => {
-                drivers.json.write(`rev-index/${versionObject.version.data.attributes.version}/public_modules/${encodeURIComponent(publicModule.attributes.name)}.json`, publicModule);
-            });
-        versionObject.publicClasses
-            .forEach(publicClass => {
-                drivers.json.write(`rev-index/${versionObject.version.data.attributes.version}/public_classes/${encodeURIComponent(publicClass.attributes.name)}.json`, publicClass);
-            })
-    }
-    // console.log('version', version.data.id);
+    logger.logGreen(`version: ${versionObject.version.data.id}, public classes: ${versionObject.publicClasses.length}, public modules: ${versionObject.publicModules.length}, methods: ${versionObject.methods.length}`);
+
+    drivers[DRIVER].write('modules', versionObject.publicModules);
+    drivers[DRIVER].write('classes', versionObject.publicClasses);
+    drivers[DRIVER].write('methods', versionObject.methods);
 })
 .catch(err => {
-    console.log('Error on global script::', err);
+    console.log('Error::', err);
 });
+
+/**
+ * Takes an array of classes, extracts the methods from each one,
+ * and runs the method schema to transform the payload
+ * @param {Array}   classes     - Array of "method" objects.
+ * @return {Array}              - Returns an array of transformed method objects
+ */
+function extractMethodsFromClasses(classes) {
+    return classes.reduce((methods, currentClass) => {
+        return currentClass.data.attributes.methods
+            .reduce((classMethods, currentMethod) => {
+                // Transform the current method and push on to methods.
+                classMethods.push(schemas.methodSchema(currentMethod));
+                return classMethods;
+            }, [])
+            // Merge all methods of all classes into a single array
+            .concat(methods);
+    }, []);
+}
