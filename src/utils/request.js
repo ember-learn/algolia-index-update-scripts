ee047513508a2001c0114c46eb2c278ad8764b88
Michael Schinis <mschinis@gmail.com>
2017-11-30 05:47:05 +0000

    feat(request): Adds comments

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,13 @@
 import rp from 'request-promise';
 import { logBlue, logRed } from 'src/utils/logger';
 
+/**
+ * Makes an HTTP request to the uri provided and returns a promise.
+ *
+ * @param {String} uri          - URL to make the request
+ * @param {Object} overrides    - Object merged in with request configuration.
+ * @returns {Promise}           - Result of the request
+ */
 export function request(uri, overrides = {}) {
     logBlue(`DOWNLOADING:: ${uri}`);
 
