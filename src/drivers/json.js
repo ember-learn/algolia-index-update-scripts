6054aabfc111eaab829bfeb43480c59a054801ec
mschinis <mschinis@gmail.com>
2018-05-24 00:00:38 +0100

    chore(index): Define the selected driver once for simplicity

--- src/drivers/json.js
+++ src/drivers/json.js
@@ -1,3 +1,4 @@
+import Bluebird from 'bluebird';
 import fs from 'file-system';
 
 export function init(fileName) {}
@@ -6,7 +7,7 @@ export function write(fileName, content) {
     return fs.writeFileSync(`./drivers-output/json/${fileName}.json`, JSON.stringify(content), 'utf8');
 }
 
-export function clear() {}
+export function clear() { return Bluebird.resolve(); }
 
 export default {
     init,
