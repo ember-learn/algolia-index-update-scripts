bf1d041bbbe96285d2a82366b733501cbe6ae7c6
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-10-19 14:12:17 +0530

    Move pkgs

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-require = require("esm")(module);
-const program = require("commander");
+require = require('esm')(module)
+const program = require('commander')
 
-const { version, description } = require("./package.json");
-const { run: runGuides } = require("./src/guides");
-const { run: runApi } = require("./src/api");
+const { version, description } = require('./package.json')
+const { run: runGuides } = require('./lib/guides')
+const { run: runApi } = require('./lib/api')
 
 program
-  .version(version, "-v, --version")
+  .version(version, '-v, --version')
   .description(description)
-  .option("-p, --project <project>", 'Project name. Accepts "api" or "guides"');
+  .option('-p, --project <project>', 'Project name. Accepts "api" or "guides"')
 
-program.on("--help", function() {
+program.on('--help', function() {
   console.log(`
     Examples:
         $ yarn start -p api
         $ yarn start -p guides
-    `);
-});
+    `)
+})
 
-program.parse(process.argv);
+program.parse(process.argv)
 
 switch (program.project) {
-  case "guides":
-    runGuides();
-    break;
-  case "api":
-    runApi();
-    break;
+  case 'guides':
+    runGuides()
+    break
+  case 'api':
+    runApi()
+    break
   default:
-    throw new Error("Invalid --project property");
+    throw new Error('Invalid --project property')
 }
