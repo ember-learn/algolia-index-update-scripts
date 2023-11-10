8bca4be4f511416710792cee11d632fd571fdca8
Chris Manson <chris@manson.ie>
2023-11-10 13:58:30 +0000

    setup linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: '2022',
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:n/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+    browser: false,
+  },
+  rules: {},
+};
